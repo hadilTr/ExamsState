7c76ee74d689bc330381ff37ad3efcc1d6b2929d
Mestaysser <mestaysserasma@gmail.com>
2025-04-02 23:39:23 +0100

    frontend update

--- front-end/src/app/Services/update.service.ts
+++ front-end/src/app/Services/update.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+
 export interface Update {
-  level?: string;
-  subject?: string;
-  validation_status?: string;
-  verification_status?: string;
+  enseignant: string;
+  nomMatiere: string;
+  departement: string;
+  niveau: string;
+  recu: boolean;
+  valide: boolean;
 }
 
-@Injectable({
-  providedIn: 'root',
-})
-export class UpdateService  {
-  private apiUrl = '';
-  constructor(private http:HttpClient) {}
 
-  getHistoriques():Observable<Update[]>{
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateService {
+  private apiUrl = 'http://localhost:8083/api/matieres/recent';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getHistoriques(): Observable<Update[]> {
     return this.http.get<Update[]>(this.apiUrl);
   }
 }
