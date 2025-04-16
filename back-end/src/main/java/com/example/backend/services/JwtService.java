ecece90f0814ac925202803c8b29dfc6ec8b6e51
hadil <hadil.trabelsi@enicar.ucar.tn>
2025-04-16 14:01:28 +0100

    added a security layer (random tokens)

--- back-end/src/main/java/com/example/backend/services/JwtService.java
+++ back-end/src/main/java/com/example/backend/services/JwtService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class JwtService {
@@ -14,11 +15,14 @@ public class JwtService {
     private final String SECRET_KEY = "your-256-bit-secret-key-of-at-least-32-characters";
 
     public String generateToken(User user) {
+
+
         return Jwts.builder()
                 .setSubject(user.getUsername())
                 .claim("role", user.getRole().toString())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .setId(UUID.randomUUID().toString()) // Unique ID for each token
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
