e8831bacec0a51c80e8f11b14b37adc5b7d0e3de
jihed <mekni.jihed2016@gmail.com>
2025-04-19 17:45:50 +0100

    add list Emails

--- back-end/src/main/java/com/example/backend/services/EmailService.java
+++ back-end/src/main/java/com/example/backend/services/EmailService.java
@@ -1,20 +1,30 @@
 package com.example.backend.services;
 
+import com.example.backend.models.EmailLog;
+import com.example.backend.repositories.EmailLogRepository;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
 
     private final JavaMailSender mailSender;
+    private final EmailLogRepository emailLogRepository; // 🆕
 
-    public EmailService(JavaMailSender mailSender) {
+    public EmailService(JavaMailSender mailSender ,  EmailLogRepository emailLogRepository) {
         this.mailSender = mailSender;
+        this.emailLogRepository = emailLogRepository;
+
     }
 
     public void sendCredentials(String toEmail, String username, String rawPassword) {
-        SimpleMailMessage message = new SimpleMailMessage();
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+
         message.setTo(toEmail);
         message.setSubject("Your ENICarthage Account Credentials");
         message.setText(
@@ -24,20 +34,44 @@ public class EmailService {
                         "Please change your password after logging in.\n"
 
         );
+
         mailSender.send(message);
+            // Sauvegarder dans EmailLog
+            EmailLog log = new EmailLog();
+            log.setToEmail(toEmail);
+            log.setSubject(message.getSubject());
+            log.setContent(message.getText());
+            log.setDateSent(LocalDateTime.now());
+            emailLogRepository.save(log);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
     public void sendNotificationToTeacher(String toEmail, String teacherName) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Notification  ENICarthage - Matières Assignées");
-        message.setText(
-                "Dear " + teacherName + ",\n\n" +
-                        ".\n\n" +
-                        "SVP rajaaa3 l examen.\n\n" +
-                        "ENICarthage Administration"
-        );
-        mailSender.send(message);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(toEmail);
+            message.setSubject("Notification ENICarthage - Matières Assignées");
+            message.setText(
+                    "Dear " + teacherName + ",\n\n" +
+                            "SVP rajaaa3 l examen.\n\n" +
+                            "ENICarthage Administration"
+            );
+            mailSender.send(message);
+
+            // Sauvegarder dans EmailLog
+            EmailLog log = new EmailLog();
+            log.setToEmail(toEmail);
+            log.setSubject(message.getSubject());
+            log.setContent(message.getText());
+            log.setDateSent(LocalDateTime.now());
+            emailLogRepository.save(log);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
